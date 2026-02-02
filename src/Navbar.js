002c4c237198f996ccc0786af66fb759ee8d1963
“cdenny15” <christian.m.denny@gmail.com>
2026-02-02 15:28:17 -0500

    fix Navbar component to switch internal pages without full page reload and add design and implementation plan

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,13 @@
-import { link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
-    return (
-      <nav className="navbar" >
-        <ul className="nav-list">
-          <li><a href="/">Home</a></li>
-          <li><a href="/game">Game</a></li>
-          <li><a href="/ranks">Ranks</a></li>
-        </ul>
-      </nav>
-
-    );
+  return (
+    <nav className="navbar">
+      <ul className="nav-list">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/game">Game</Link></li>
+        <li><Link to="/ranks">Ranks</Link></li>
+      </ul>
+    </nav>
+  );
 }
